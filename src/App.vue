c52accf6e7c3269b270cf7950fcbcba5f7a8d116
enzo.luo <duenzo1010@gmail.com>
2024-01-09 08:52:25 +0800

    build: init project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+  <div class="flex justify-center items-center">
+    <RouterView />
+  </div>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
+</script>
+
+<template>
+  <div class="h-screen flex justify-center items-center">
+    <Card class="w-[350px]">
+      <CardHeader class="text-center">
+        <CardTitle>Login Form</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div class="grid items-center w-full gap-4">
+            <div class="flex flex-col space-y-1.5">
+              <Label for="account">Account</Label>
+              <Input id="account" placeholder="Please input account" />
+            </div>
+            <div class="flex flex-col space-y-1.5">
+              <Label for="password">Password</Label>
+              <Input id="password" placeholder="Please input password" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter class="flex justify-between px-6 pb-6">
+        <Button type="button" class="w-full">Login</Button>
+      </CardFooter>
+    </Card>
+  </div>
+</template>
